b037540b814e5831b44f0df47e87280c321ce9f6
Donnie <Donnell.Jackson@outlook.com>
2021-03-18 00:57:07 -0500

    updating visualiazation map with leaflet.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,18 +18,19 @@ d3.csv("https://raw.githubusercontent.com/DonnieData/austin_crime_EDA/main/loc_f
             
         };
     });
-
+    // test object structure 
     console.log(latLong);
     console.log(latLong[16])
     console.log(latLong[16]['latitude'])
 
+    // create initial map view 
     var map = L.map('mapid').setView([30.26548193884994, -97.74588441015467], 13);
-
+    // create map layer 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-
+    // loop through location object and create markers 
     for (var i = 0; i < locData.length; i++) {
         marker = new L.marker([locData[i]['latitude'], locData[i]['longitude']])
         .bindPopup(locData[i]['incident_report_number'])
