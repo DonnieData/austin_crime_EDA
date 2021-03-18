2fd202943ef44a1a913d2faa379c7a218f45f1c1
Donnie <Donnell.Jackson@outlook.com>
2021-03-18 14:23:23 -0500

    testing adding layers and overlay to map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,9 +1,44 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+//Map Setup
+
+// create map layer 
+let streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+// Create map object and add default layer
+let map = L.map('mapid', {
+	center: [30.26548193884994, -97.74588441015467],
+	zoom: 13,
+	layers: [streets]
+});
+
+// create base to hold map layers
+let baseMap = {
+    "Streets":streets
+};
+
+//create overlay objects for map 
+let samplayer1 = new L.LayerGroup();
+let samplayer2 = new L.LayerGroup();
+let samplayer3 = new L.LayerGroup();
+
+// create refernece to overlays 
+let overlays = {
+    "sample layer 1": samplayer1,
+    "sample layer 2": samplayer2,
+    "sample layer 3": samplayer3
+};
+
+//add control to to map for user-end toggling 
+L.control.layers(baseMap, overlays).addTo(map);
+
+
+//Data
+
 //load data 
 d3.csv("https://raw.githubusercontent.com/DonnieData/austin_crime_EDA/main/loc_frame.csv").then(function(data) {
-    const locData =  data;
+    var locData =  data;
 
     //test output 
     console.log(locData[0]);
@@ -23,26 +58,18 @@ d3.csv("https://raw.githubusercontent.com/DonnieData/austin_crime_EDA/main/loc_f
     console.log(latLong[16])
     console.log(latLong[16]['latitude'])
 
-    // create initial map view 
-    var map = L.map('mapid').setView([30.26548193884994, -97.74588441015467], 13);
-    // create map layer 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // function to style markers
     var styleInfo = {
-    
             opacity: 1,
-            radius: 8,
-            
-       
+            radius: 8 
     }
 
     // loop through location object and create markers 
     for (var i = 0; i < locData.length; i++) {
         marker = new L.circle([locData[i]['latitude'], locData[i]['longitude']],styleInfo)
         .bindPopup(locData[i]['incident_report_number'])
-        .addTo(map);
+        .addTo(samplayer2);
     };
 
 });
-
