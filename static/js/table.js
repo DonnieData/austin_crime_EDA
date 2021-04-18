eeb7c504525ce726262947ae65e14a0e79c5574f
Donnell Jackson <Donnell.Jackson@outlook.com>
2021-04-18 14:57:58 -0500

    building out javascript code for tables, updating bootstrap csss for tables, adding repsonsiveness to tables, updating web app layout

--- static/js/table.js
+++ static/js/table.js
@@ -1,17 +1,10 @@
 console.log("working");
 
-var table1 = d3.select("#sampletab1");
 
-    function buildTable(data) {
-       // let header = Object.keys(data[0]) 
-       table1.append("tr")
-        Object.keys(data[0]).forEach((label) => {
-            let header = table1.append("th")
-            header.text(label);
-        })
-
+    function buildTable(data,tableId) {
+        var table = d3.select(tableId)
         data.forEach((dataRow) => {
-            let row = table1.append("tr");
+            let row = table.append("tr");
 
             Object.values(dataRow).forEach((val) => {
                 let cell = row.append("td")
@@ -22,13 +15,15 @@ var table1 = d3.select("#sampletab1");
     )};
     
 
-
+//map table_1
 var url = "https://raw.githubusercontent.com/DonnieData/austin_crime_EDA/main/datasets/sample_table.csv"
 d3.csv(url).then(function(data) {
 
     console.log(data[0])
     console.log(data[2])
 
-    buildTable(data)    
-
+    buildTable(data,"#sampletab1")    
+    buildTable(data,"#sampletab2")
 });
+
+//map table_2 
